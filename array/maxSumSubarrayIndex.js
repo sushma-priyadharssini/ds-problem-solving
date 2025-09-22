fccf5388fb4cd7172e002e09cb2e05c4e5222ec9
Sushma Priyadharssini KV <kvsushmapriyadharssini@gmail.com>
2025-09-22 18:42:57 +0530

    sliding window - max subarray sum index

--- array/maxSumSubarrayIndex.js
+++ array/maxSumSubarrayIndex.js
@@ -1,22 +1,32 @@
-let arr = [2, -4, 5, -6, -7, 8, 9, -1, 10];
+function maxSubarrayIndices(arr) {
+    let maxSum = -Infinity;
+    let currentSum = 0;
 
-const calculateMaxSum = (array) => {
-    let maxSum = 0;
-    let l = 0;
-    let sum = 0;
-    let maxSumIndex = [0, 0];
+    let start = 0;
+    let tempStart = 0;
+    let end = 0;
 
-    for (let r = 0; r < array.length; r++) {
-        sum += array[r];
-        if (sum >= maxSum) {
-            maxSum = sum;
-            maxSumIndex = [l, r];
+    for (let i = 0; i < arr.length; i++) {
+        if (currentSum <= 0) {
+            currentSum = arr[i];
+            tempStart = i;
         } else {
-            sum -= array[l];
-            l++;
+            currentSum += arr[i];
+        }
+
+        if (currentSum > maxSum) {
+            maxSum = currentSum;
+            start = tempStart;
+            end = i;
         }
     }
-    return maxSumIndex;
-};
 
-console.log(calculateMaxSum(arr));
+    return { start, end, subarray: arr.slice(start, end + 1) };
+}
+
+// Example
+// const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// const arr = [-10, -9, -2, -1, 0];
+const arr = [2, -4, 5, -6, -7, 8, 9, -1, 10];
+console.log(maxSubarrayIndices(arr));
+
